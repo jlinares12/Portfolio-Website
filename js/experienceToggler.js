562315d2fe4955dc3eecf63c706f327cb7e94a94
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-05-06 18:17:32 -0700

    Added local storage data to check which experience section was last checked

--- js/experienceToggler.js
+++ js/experienceToggler.js
@@ -4,16 +4,31 @@ var education = document.querySelector(".education");
 var project_content = document.querySelector(".project-border");
 var education_content = document.querySelector(".education-border");
 
-function projectsToggle(){-
+var experience = localStorage.getItem("experience");
+
+window.addEventListener("load", () => {
+    if( experience == "education" ) {
+        projects.classList.add("inactive");
+        education.classList.remove("inactive");
+        education_content.classList.remove("hide");
+        project_content.classList.add("hide")
+    }
+  })
+
+function projectsToggle(){
     education.classList.add("inactive");
     projects.classList.remove("inactive");
     project_content.classList.remove("hide");
-    education_content.classList.add("hide")
+    education_content.classList.add("hide");
+
+    localStorage.setItem("experience", "projects");
 }
 
-function educationToggle() {-
+function educationToggle() {
     projects.classList.add("inactive");
     education.classList.remove("inactive");
     education_content.classList.remove("hide");
     project_content.classList.add("hide")
+
+    localStorage.setItem("experience", "education");
 }
